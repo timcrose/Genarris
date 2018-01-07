459fd471cbcf2072f047b75b84e02c9dd8e3032e
timcrose <timcrose09@gmail.com>
2018-01-07 14:29:46 -0500

    Add files via upload

---
+++ run_isgep.py
@@ -0,0 +1,8 @@
+#Place this script in any of the example folder to run ISGEP
+
+import os
+
+working_dir = os.getcwd()
+configuration = os.path.join(os.getcwd(),"ui.conf")
+isgep_path = os.path.join(os.path.dirname(os.path.dirname(working_dir)),"src/isgep_master.py")
+os.system("python "+isgep_path+" "+configuration)
