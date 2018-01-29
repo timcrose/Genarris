4f91dd17d96b1ee73ce1f88b72f274ec8c1f15da
Xiayue Li <xli20@tulane.edu>
2018-01-28 23:22:52 -0800

    Code clean up

--- src/evaluation/affinity.py
+++ src/evaluation/affinity.py
@@ -8,9 +8,9 @@ import os
 from sklearn.cluster import AffinityPropagation
 from sklearn.metrics import silhouette_score
 import numpy as np
-from utilities import misc
 from utilities.misc import output_pool
-from utilities.write_log import print_time_log, write_log
+from utilities.write_log import print_time_log, write_log, \
+    print_time_warning
 from evaluation.evaluation_util import PoolOperation, \
         load_pool_operation_keywords
 
@@ -34,7 +34,8 @@ def affinity_propagation_analyze_preference(inst):
 
 def affinity_propagation_fixed_clusters(inst):
     '''
-    AP that explores the setting of preference in order to generate desired number of clusters
+    AP that explores the setting of preference in order to generate
+    desired number of clusters
     '''
     sname = "affinity_propagation_fixed_clusters"
     num_of_clusters = inst.get_eval(sname,"num_of_clusters")
@@ -72,127 +73,6 @@ def affinity_propagation_fixed_silhouette(inst):
             target_silhouette, preference_list,
             silhouette_tolerance=silhouette_tolerance,
             output_without_success=output_without_success)
-
-
-def AP_distance_matrix(coll, dist_mat, affinity_type=["exponential",1],
-                       damping=0.5, convergence_iter=15, max_iter=200,
-                       preference=None, stored_property_key="AP_cluster"):
-    '''
-    Given a collection of structures and their distance matrix
-    Conduct the Affinity Propagation on the collection
-    '''
-    affinity_mat = _convert_distance_to_affinity_mat(dist_mat, affinity_type)
-    return AP_affinity_matrix(coll, affinity_mat, damping=damping,
-                              convergence_iter=convergence_iter, 
-                              max_iter=max_iter, preference=preference,
-                              stored_property_key=stored_property_key)
-
-
-def AP_affinity_matrix(coll, affinity_mat, damping=0.5, convergence_iter=15,
-                       max_iter=200, preference=None, stored_property_key="AP_cluster"):
-    '''
-    Given a collection of structure and their affinity matrix
-    Conduct the Affinity Propagation on the collection
-    Returns a full list of collection with cluster assigned,
-    as well as a list of exemplars
-    '''
-    ap = AffinityPropagation(damping=damping, max_iter=max_iter, 
-                             convergence_iter=convergence_iter,
-                             copy=True, preference=preference,
-                             affinity="precomputed",verbose=False)
-
-    
-    result = ap.fit(affinity_mat)
-    for x in range(len(coll)):
-        coll[x].properties[stored_property_key] = result.labels_[x]
-
-    centers = [coll[x] for x in result.cluster_centers_indices_]
-    return coll, centers
-
-def AP_vector_list(coll, vector_list, damping=0.5, convergence_iter=15,
-                   max_iter=200, preference=None, stored_property_key="AP_cluster"):
-    '''
-    Given a collection of structure and their feature vectors
-    Conduct the Affinity Propagation on the collection
-    Returns a full list of collection with cluster assigned,
-    as well as a list of exemplars
-    '''
-    ap = AffinityPropagation(damping=damping, max_iter=max_iter, 
-                             convergence_iter=convergence_iter,
-                             copy=True, preference=preference,
-                             affinity="euclidean",verbose=False)
-
-   
-
-
-def _convert_distance_to_affinity_mat(dist_mat, affinity_type=["exponential",1]):
-    '''
-    Converts a given distance matrix to affinity matrix
-    '''
-    m = len(dist_mat); n = len(dist_mat[0])
-    affinity_mat = [[0]*n for x in range(m)]
-
-    for i in range(m):
-        for j in range(n):
-            if affinity_type[0]=="exponential":
-                affinity_mat[i][j] = - np.exp(dist_mat[i][j]*affinity_type[1])
-            elif affinity_type[0]=="power":
-                affinity_mat[i][j] = - dist_mat[i][j]**affinity_type[1]
-
-    return affinity_mat
-
-def _generate_preference_array(coll, pref):
-    '''
-    Generate preference array according to preference
-    '''
-    if pref==None:
-        return None
-    if pref[0]=="constant":
-        return [pref[1]]*len(coll)
-
-
-def _print_ap_cluster_information(centers, dists, coll, stored_property_key, dist_mat):
-    '''
-    Generates standard cluster information
-    '''
-    st = ""
-    st += "Number of clusters generated: %i\n" % len(centers)
-    st += "Average distance to centers: %f\n" % np.mean(dists)
-    st += "STD of distance to centers: %f\n" % np.std(dists)
-
-    labels = np.array([struct.properties[stored_property_key] for struct in coll])
-    llabels = list(labels)
-    n_struct = [llabels.count(struct.properties[stored_property_key]) for struct in centers]
-    
-    st += "STD of number of structures in clusters: %f\n" % np.std(n_struct)
-
-    try:
-        sil_score = silhouette_score(np.array(dist_mat),labels,metric="precomputed") 
-    except:
-        sil_score = 1.0
-    st += "Silhouette score: %f\n\n" % sil_score 
-
-
-    st += "Center and number of structures of each cluster:\n" 
-
-    labels = list(labels)
-    for struct in centers:
-         st += "Cluster %i: %s %i\n" % (struct.properties[stored_property_key],
-                               struct.struct_id,
-                               labels.count(struct.properties[stored_property_key]))
-
-
-    st += "\n"
-    st += "Assigned cluster label and distance to center:\n"
-    st += "\n".join(map(str,[struct.struct_id + " " + 
-                             str(struct.properties[stored_property_key])+" "+
-                             str(dist_mat[coll.index(struct)]
-                             [coll.index(centers[struct.properties[stored_property_key]])])
-                             for struct in coll]))
-    st += "\n"
-
-    return st
-
 
 def get_affinity_propagation_executor(inst, section):
     dist_mat_input_file = inst.get(section, "dist_mat_input_file")
