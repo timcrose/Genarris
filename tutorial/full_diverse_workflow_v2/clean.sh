e159081e59be872ff9fd39e544a6e33cd2d1dd18
timcrose <timcrose09@gmail.com>
2019-07-16 18:14:07 -0400

    added tutorial example

---
+++ tutorial/full_diverse_workflow_v2/clean.sh
@@ -0,0 +1,4 @@
+rm Genarris.* j_*.out
+#rm -r aims_target1_2mpc_he harris_single_molecule_prep_aims_dir structure_dir_for_harris_single_molecule_prep target1_2mpc_he
+#rm -r aims_relaxation_single structure_dir_for_relaxing_single_molecule
+rm -r aims_target1_2mpc_he
